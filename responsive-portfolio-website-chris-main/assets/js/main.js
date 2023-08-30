6c13fc60113187466cdabe18acdcadb13819bfcb
priyanshu-vaghela <priyanshuvaghela219@gmail.com>
2023-08-30 10:44:57 +0530

    priyanshu
    About

---
+++ responsive-portfolio-website-chris-main/assets/js/main.js
@@ -0,0 +1,41 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== SWIPER PROJECTS ===============*/
+let swiperProject = new Swiper(".projects__container", {
+    loop: true,
+    spaceBetween: 24,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    breakpoints: {
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: -56,
+        }
+    }
+})
+/*=============== SWIPER TESTIMONIAL ===============*/
+/*=============== EMAIL JS ===============*/
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+/*=============== SHOW SCROLL UP ===============*/
+
+
+
+/*=============== DARK LIGHT THEME ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
